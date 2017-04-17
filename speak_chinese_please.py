25b81b652674068f77bd1193225ed6c3753e28ad
migege <lzw.whu@gmail.com>
2017-04-17 17:42:22 +0800

    rename

---
+++ speak_chinese_please.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+import sys
+import json
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
+def any_chinese(s):
+    return any(u'\u4e00' <= c <= u'\u9fff' for c in s.decode('utf-8'))
+
+
+def register():
+    hooks = ['PreComment']
+    print json.dumps(hooks)
+
+
+def PreComment(content):
+    print 0 if any_chinese(content) else 1
